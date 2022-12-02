f3f5a0db39aa614a9b50d7a03edf5b6dcaec67e3
SyoutaYamamoto <s.yamamoto6174@gmail.com>
2022-12-02 18:05:06 +0900

    ホームのカレンダー表示

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Event;
+use App\Models\User;
 
 class HomeController extends Controller
 {
     //
     public function home()
     {
-        return inertia("Home/Home");
+        return inertia("Home/Home",["users"=>User::all(),"events" =>  Event::where("user_id",auth()->id())->get()]);
+    }
+    public function reload(Request $request)
+    {
+
+        $id = $request->input("id");
+        //dd($id);
+        return inertia("Home/Home",["users"=>User::all(),"events" =>  Event::where("user_id",$id)->get()]);
     }
 }
--- resources/js/Pages/Home/Home.jsx
+++ resources/js/Pages/Home/Home.jsx
@@ -1,32 +1,66 @@
 import React, {useState} from "react";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import jaLocale from '@fullcalendar/core/locales/ja';
 import { Inertia } from "@inertiajs/inertia";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
-import { Link } from '@inertiajs/inertia-react'
+import { Link, useForm } from '@inertiajs/inertia-react'
 
-const Home = () => {
-
+const Home = (props) => {
+    console.log(props);
+    const {users,auth,events} = props;
+    const {data, setData, post} = useForm({
+        id: "",
+    })
+const handleSendPosts = (e) => {
+        e.preventDefault();
+        post("/home");
+    }
     return (
-    <>
+    <Authenticated auth={props.auth} header={
+    <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    ホーム
+                </h2>
+            }>
       <p>home</p>
       <p>こんにちは</p>
       <div>
-           <Link href="/mypage">Mypageへ</Link>
-           <br />
-           <Link href="/admin">Adminへ</Link>
-           <br />
-           <Link href="/schedule">Scheduleへ</Link>
-           <br />
-           <Link href="/content">Contentへ</Link>
-           <br />
-           <Link href="/message">Messageへ</Link>
-           <br />
-           <Link href="/info">Infoへ</Link>
-           <br />
-           <Link href="/chat">Chatsへ</Link>
-           <br />
-           <Link href="/posts">伝言板へ</Link>
-       </div>
-    </>
+      <form onSubmit={handleSendPosts}>
+            <select onChange={e => setData("id", e.target.value)}>
+            {users.map((user) => (
+                <option value={user.id}>{user.name}</option>
+                        ))}
+            </select>
+            <button type="submit" className="p-1 bg-purple-300 hover:bg-purple-400 rounded-md">send</button>
+            </form>
+        <div className="px-36">
+        
+        <FullCalendar 
+            plugins={[dayGridPlugin]} 
+            initialView="dayGridMonth"
+            height={400}
+            locales={[jaLocale]}
+            locale='ja'
+            events={events}
+        />
+        </div>
+        <Link href="/mypage">Mypageへ</Link>
+        <br />
+        <Link href="/admin">Adminへ</Link>
+        <br />
+        <Link href="/schedule">Scheduleへ</Link>
+        <br />
+        <Link href="/content">Contentへ</Link>
+        <br />
+        <Link href="/message">Messageへ</Link>
+        <br />
+        <Link href="/info">Infoへ</Link>
+        <br />
+        <Link href="/chat">Chatsへ</Link>
+        <br />
+        <Link href="/posts">伝言板へ</Link>
+    </div>
+    </Authenticated>
   );
 }
 
